b292f707110f33c53eba3ce273f5711e6d145d7e
JuanBarbosaMora <portelnom0308@gmail.com>
2025-01-30 18:55:13 -0500

    Agregando funcionalidades al juego | Fin creacion juego

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let listaAmigos = []
 
 function agregarAmigo() {
     let capturarAmigo = document.querySelector("#nombreAmigo").value.trim();
+
     if (capturarAmigo === "" || !/^[a-zA-ZñÑ\s]+$/.test(capturarAmigo)) {
         alert("Por favor, inserte un nombre valido.");
     } else {
@@ -20,4 +21,40 @@ function mostrarAmigos() {
         li.textContent = listaAmigos[x];
         amigosSeleccionados.appendChild(li);
     }
+}
+
+function sortearAmigo() {
+    const listaAmigos = getListaAmigos();
+
+    if (listaAmigos.length === 0) {
+        mostrarMensaje("No hay amigos por sortear.");
+        return;
+    }
+
+    const amigoGanador = document.querySelector("#resultado");
+    amigoGanador.innerHTML = "";
+    const numeroSorteo = Math.floor(Math.random() * listaAmigos.length);
+    const li = document.createElement("li");
+    li.textContent = listaAmigos[numeroSorteo];
+    amigoGanador.appendChild(li); 
+    mostrarMensaje(""); // Borra mensaje despues de un sorteo valido
+    deshabilitarSorteo();
+}
+
+// Muestra mensajes de error
+function mostrarMensaje(mensaje) {
+    const mensajeElemento = document.querySelector("#mensaje");
+    mensajeElemento.textContent = mensaje;
+    mensajeElemento.style.display = "block";
+    mensajeElemento.style.color = "red";
+    mensajeElemento.style.fontWeight = "bold";
+}
+
+function deshabilitarSorteo() {
+    document.querySelector("#botonSorteo").setAttribute("disabled", "true");
+}
+
+// Obtiene los amigos sin ser mostrados
+function getListaAmigos() {
+    return listaAmigos;
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+🎉 ¡Bienvenido a mi Proyecto de Aprendizaje! 🎉
+
+¡Hola y bienvenido! 👋
+
+Este proyecto es un espacio donde aprendo y reaprendo conceptos de programación, reforzando buenas prácticas y evitando malos hábitos. Es mi manera de recordar por qué amo el desarrollo y seguir mejorando constantemente.
+
+Además, quiero agradecer a Alura Latam por compartir su conocimiento y ayudarme en este proceso de crecimiento. 🙌
+
+🔍 ¿De qué trata este proyecto?
+Se trata de un juego del Amigo Secreto, donde los participantes pueden descubrir de forma interactiva a quién les tocó regalar. ¡Una forma divertida de aplicar lo aprendido y compartir con otros! 🎁
+
+Espero que disfrutes explorando este proyecto tanto como yo disfruté creándolo. ¡Nos vemos en el código! 🚀
