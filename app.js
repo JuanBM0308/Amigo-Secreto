64b051a7756a63acf5f8221ea8f35d4b44c90c3b
JuanBarbosaMora <portelnom0308@gmail.com>
2025-01-29 20:28:08 -0500

    Implementando funcion captar amigo y agregar amigo a una lista

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//Logica desafio
+let listaAmigos = []
+
+function agregarAmigo() {
+    let capturarAmigo = document.querySelector("#nombreAmigo").value.trim();
+    if (capturarAmigo === "" || !/^[a-zA-ZñÑ\s]+$/.test(capturarAmigo)) {
+        alert("Por favor, inserte un nombre valido.");
+    } else {
+        listaAmigos.push(capturarAmigo);
+        mostrarAmigos();
+        document.querySelector("#nombreAmigo").value = "";
+    }
+}
+
+function mostrarAmigos() {
+    let amigosSeleccionados = document.querySelector("#listaAmigos");
+    amigosSeleccionados.innerHTML = "";
+
+    for (let x = 0; x < listaAmigos.length; x++) {
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[x];
+        amigosSeleccionados.appendChild(li);
+    }
+}
